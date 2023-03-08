10b20506c11d9a82a22c75ec8c4026ff310441ea
MaksKatsenko <124710290+MaksKatsenko@users.noreply.github.com>
2023-03-08 19:42:38 +0200

    HW_9

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,88 +1,48 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistrationTests {
-    WebDriver wd;
-    String IncorrectEmail = "arcdef.com";
-    String IncorrectPassword = "1234";
+public class RegistrationTests extends TestBase {
+    String CorrectEmail = "Telran2022@gmail.com";
+    String CorrectPassword = "Ads$gf1288";
+    String IncorrectEmail = "ran2018mail.com";
+    String IncorrectPassword = "ds1288";
 
-    @BeforeMethod
-    public void init(){
-        wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-
-    }
+ @BeforeMethod
+ public void preCondition(){
+     if (isLogged()){
+         logout();
+     }
+ }
 
     @Test
     public void regPositiveTest() {
-        WebElement loginBtn = wd.findElement(By.xpath("//a[@href='/login']"));
-        loginBtn.click();
-
+       openLoginRegistrationForm();
         int i = (int)(System.currentTimeMillis() /1000) % 3600;
-
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("arc" + i + "@def.com");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("$Altcd12434");
-
-        wd.findElement(By.xpath("//button[2]")).click();
-
+        String email = "arc" + i + "@def.com";
+        String password = "$Altcd12434";
+        fillLoginRegistrationForm(email, password);
+        submitRegistration();
         Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
     }
 
     @Test
     public void regNegativeTest() {
-        WebElement loginBtn = wd.findElement(By.xpath("//a[@href='/login']"));
-        loginBtn.click();
-
+        openLoginRegistrationForm();
         int i = (int)(System.currentTimeMillis() /1000) % 3600;
-
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys(IncorrectEmail);
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("Rtg$tcd12434");
-
-        wd.findElement(By.xpath("//button[2]")).click();
-
-        Assert.assertFalse(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+        fillLoginRegistrationForm(IncorrectEmail, CorrectPassword);
+        submitRegistration();
     }
 
     @Test
     public void regNegativeTest2() {
-        WebElement loginBtn = wd.findElement(By.xpath("//a[@href='/login']"));
-        loginBtn.click();
-
+        openLoginRegistrationForm();
         int i = (int)(System.currentTimeMillis() /1000) % 3600;
-
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("arc" + i + "@def.com");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys(IncorrectPassword);
-
-        wd.findElement(By.xpath("//button[2]")).click();
-
-        Assert.assertFalse(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+        fillLoginRegistrationForm(CorrectEmail, IncorrectPassword);
+        submitRegistration();
     }
 
     @AfterMethod
